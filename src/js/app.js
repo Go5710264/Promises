b1e305d25143188a8dd2760b231abd485bb0685c
Dima Shirkov <dimashirkov@list.ru>
2022-12-27 19:48:39 +0300

    formatting test.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import GameSavingLoader from './GameSavingLoader';
 
 GameSavingLoader.load().then((saving) => {
   const GameSaving = JSON.parse(saving);
+  return GameSaving;
 }, (error) => {
   console.log(error);
 });
